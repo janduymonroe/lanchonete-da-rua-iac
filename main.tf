981869ca3c07cdee7245fa27d07ba070fd027d71
Janduy Monroe <66971685+janduymonroe@users.noreply.github.com>
2023-11-07 15:14:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,28 @@ resource "aws_lambda_function" "generate_token" {
   source_code_hash = null
 }
 
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_execution_role" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_exec.name
+}
+
 ### GATEWAYs
 
 resource "aws_api_gateway_rest_api" "lanchonetedarua" {
